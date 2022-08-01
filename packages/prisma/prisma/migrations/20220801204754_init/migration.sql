c0749162727489eca71a6d54933bf86708478550
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-08-02 05:05:48 +0800

    upd

--- packages/prisma/prisma/migrations/20220801204754_init/migration.sql
+++ packages/prisma/prisma/migrations/20220801204754_init/migration.sql
@@ -13,9 +13,9 @@ CREATE TABLE "Auth" (
 -- CreateTable
 CREATE TABLE "Message" (
     "id" SERIAL NOT NULL,
-    "userd" TEXT NOT NULL,
-    "username" TEXT NOT NULL,
-    "text" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "userName" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
 
     CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
 );
